bc71406fae235e03c142b5272f19fbdb1cad1185
Judy Mwai <juddiemukami@gmail.com>
2022-10-26 00:12:57 +0300

    first commit

---
+++ MyJokesApp/Models/Joke.cs
@@ -0,0 +1,15 @@
+﻿namespace MyJokesApp.Models
+{
+    public class Joke
+        
+
+    {
+        public int id { get; set; }
+        public string JokeQuestion { get; set; }
+        public string JokeAnswer { get; set; }
+        public Joke()
+        {
+
+        }
+    }
+}
